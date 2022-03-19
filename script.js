c74ab22a8dc5c1f933a9fb1369cc0c6fafaeae30
Aryan Tayal <bookworms@ariansh.com>
2022-03-19 19:00:26 +0530

    create animation with GSAP

---
+++ script.js
@@ -0,0 +1,53 @@
+const btns = document.querySelectorAll("button");
+const cancelBtn = document.querySelector("#cancel");
+const confirmBtn = document.querySelector("#confirm");
+const face = document.querySelector("#face");
+
+confirmBtn.addEventListener("click", () => {
+  btns.forEach((btn) => btn.classList.add("disabled"));
+  const tl1 = gsap.timeline();
+  tl1
+    .to(btns, {
+      height: "150px",
+    })
+    .to(btns, {
+      width: "400px",
+    })
+    .to(btns, {
+      background: "#d5e68d",
+    })
+    .to(btns, {
+      width: "250px",
+      height: "500px",
+      borderRadius: (i, el) => gsap.getProperty(el, "--br"),
+    })
+    .to(".face .mouth", {
+      borderBottomLeftRadius: "50rem",
+      borderBottomRightRadius: "50rem",
+      animation: "mouth 1s ease infinite alternate",
+    });
+});
+cancelBtn.addEventListener("click", () => {
+  btns.forEach((btn) => btn.classList.add("disabled"));
+  const tl2 = gsap.timeline();
+  tl2
+    .to(btns, {
+      height: "150px",
+    })
+    .to(btns, {
+      width: "400px",
+    })
+    .to(btns, {
+      background: "#e21a1a66",
+    })
+    .to(btns, {
+      width: "250px",
+      height: "500px",
+      borderRadius: (i, el) => gsap.getProperty(el, "--br"),
+    })
+    .to(".face .mouth", {
+      borderTopRightRadius: "50rem",
+      borderTopLeftRadius: "50rem",
+      animation: "mouth 1s ease infinite alternate",
+    });
+});
